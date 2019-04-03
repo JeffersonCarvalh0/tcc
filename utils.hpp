69e52b67fd1c74ad11b4cecd2228d6bc8a6f2ea2
Jefferson <jeffersonhcarvalho@gmail.com>
2019-04-03 15:02:01 -0300

    tried to fix the indent

--- utils.hpp
+++ utils.hpp
@@ -9,62 +9,62 @@
 
 /* Tuples will be assigned to periods */
 struct Tuple {
-    int label, teacher, subject, grade;
+	int label, teacher, subject, grade;
 
-    Tuple(int label = -1, int teacher = -1, int subject = -1, int grade = -1):
-        label(label), teacher(teacher), subject(subject), grade(grade) {}
+	Tuple(int label = -1, int teacher = -1, int subject = -1, int grade = -1):
+		label(label), teacher(teacher), subject(subject), grade(grade) {}
 };
 
 struct Chromossome {
-    std::vector<std::list<Tuple>> periods;
-    std::vector<std::vector<bool>> teacher_subject;
-    std::vector<int> tc_cur_workloads;
+	std::vector<std::list<Tuple>> periods;
+	std::vector<std::vector<bool>> teacher_subject;
+	std::vector<int> tc_cur_workloads;
 
-    Chromossome(int periods_size, int teachers_size, int subjects_size) {
-        periods.resize(periods_size);
-        teacher_subject.resize(teachers_size, std::vector<bool>(subjects_size, false));
-        tc_cur_workloads.resize(subjects_size);
-    }
+	Chromossome(int periods_size, int teachers_size, int subjects_size) {
+		periods.resize(periods_size);
+		teacher_subject.resize(teachers_size, std::vector<bool>(subjects_size, false));
+		tc_cur_workloads.resize(subjects_size);
+	}
 };
 
 /* This funciton will create the tuples that needs to be assigned for the first GA */
 std::vector<Tuple> createTuples(int teacher, std::set<int> &chosen_subjects, std::map<int, int> &subjects, std::vector<int> &workloads) {
-    std::vector<Tuple> tuples;
+	std::vector<Tuple> tuples;
 
-    int total = 0;
-    for (auto &e : chosen_subjects) total += workloads[e];
-    tuples.reserve(total);
+	int total = 0;
+	for (auto &e : chosen_subjects) total += workloads[e];
+	tuples.reserve(total);
 
-    int cur_label = 0;
-    for (auto &e : chosen_subjects) {
-        for (int i = 0; i < workloads[e]; ++i)
-            tuples.push_back(Tuple(cur_label++, teacher, e, subjects[e]));
-    }
+	int cur_label = 0;
+	for (auto &e : chosen_subjects) {
+		for (int i = 0; i < workloads[e]; ++i)
+			tuples.push_back(Tuple(cur_label++, teacher, e, subjects[e]));
+	}
 
-    return tuples;
+	return tuples;
 }
 
 /* Converts output of GA1 on input of GA2 */
 std::vector<Chromossome> GA1toGA2(
-    std::vector<std::vector<int>> &input_periods, std::vector<Tuple> &tuples, std::vector<std::vector<int>> &prefs,
-    int sbj_num, int tc_num, int periods_size = 30, int pop_size = 50
-) {
-    std::vector<Chromossome> population(pop_size, Chromossome(periods_size, tc_num, sbj_num));
-    int sols_per_tc = pop_size / tc_num;
+		std::vector<std::vector<int>> &input_periods, std::vector<Tuple> &tuples, std::vector<std::vector<int>> &prefs,
+		int sbj_num, int tc_num, int periods_size = 30, int pop_size = 50
+		) {
+	std::vector<Chromossome> population(pop_size, Chromossome(periods_size, tc_num, sbj_num));
+	int sols_per_tc = pop_size / tc_num;
 
 	// Placing the tuples of GA1 at GA2
-    for (int i = 0; i < tc_num; ++i) {
-        for (int j = i * sols_per_tc; j < (i + 1) * sols_per_tc; ++j) {
+	for (int i = 0; i < tc_num; ++i) {
+		for (int j = i * sols_per_tc; j < (i + 1) * sols_per_tc; ++j) {
 			for (int z = 0; z < periods_size; ++z) {
 				if (tuples[input_periods[i][z]].label != -1)
 					population[j].periods[z].push_back(tuples[input_periods[i][z]]);
 			}
-        }
-    }
-	
+		}
+	}
+
 	// Set the remaining subjects to other teachers
-    std::random_device rd;
-    std::default_random_engine generator(rd());
+	std::random_device rd;
+	std::default_random_engine generator(rd());
 	std::uniform_int_distribution<int> random_teacher(0, tc_num - 1);
 	for (auto &chromossome : population) {
 		for (int sbj_id = 0; sbj_id < sbj_num; ++sbj_id) {
@@ -113,7 +113,7 @@ std::vector<Chromossome> GA1toGA2(
 				chromossome.teacher_subject[random_teacher(generator)][sbj_id] = true;
 		}
 	}
-	
+
 	// Set teacher workloads 
 	for (auto &chromossome : population) {
 		for (int tc_id = 0; tc_id < tc_num; ++tc_id) {
